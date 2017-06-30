cc623a0b7a1dc3d2ade9332f8952e82c01e47635
touktw <touktw@gmail.com>
2017-06-30 14:26:59 +0900

    null 체크

--- SearchNShop_and/app/src/main/java/co/esclub/searchnshop/adapter/RecyclerAdapter.kt
+++ SearchNShop_and/app/src/main/java/co/esclub/searchnshop/adapter/RecyclerAdapter.kt
@@ -119,25 +119,27 @@ class RecyclerAdapter(private val context: Context, val messenger: Messenger,
 
         viewHolder.imgButtonUpdate.tag = viewHolder
         viewHolder.imgButtonUpdate.setOnClickListener { v ->
-            Log.d("###", "time:" + (System.currentTimeMillis() - item.lastSearchTime))
-            if (System.currentTimeMillis() - item.lastSearchTime < Const.SYNC_TIMEOUT_MILLIS) {
-                Toast.makeText(context, R.string.allow_sync_after_10min, Toast.LENGTH_LONG).show()
-                return@setOnClickListener
+            item?.let {
+                Log.d("###", "time:" + (System.currentTimeMillis() - it.lastSearchTime))
+                if (System.currentTimeMillis() - it.lastSearchTime < Const.SYNC_TIMEOUT_MILLIS) {
+                    Toast.makeText(context, R.string.allow_sync_after_10min, Toast.LENGTH_LONG).show()
+                    return@setOnClickListener
+                }
+                val searchItems = ArrayList<SearchItem>()
+                searchItems.add(SearchItem(it.keyWord, it.mallName))
+                NShopSearch.search(searchItems, object : NShopSearch.Listener {
+                    override fun onPrepare() {
+                        viewHolder.itemView?.findViewById(R.id.imgButtonUpdate)
+                                ?.startAnimation(AnimationUtils.loadAnimation(context, R.anim.rotate))
+                    }
+
+                    override fun onComplete(results: List<SearchItem>?) {
+                        viewHolder.itemView?.findViewById(R.id.imgButtonUpdate)?.clearAnimation()
+                        SearchItemRepository.saveAll(results)
+                    }
+
+                })
             }
-            val searchItems = ArrayList<SearchItem>()
-            searchItems.add(SearchItem(item?.keyWord, item?.mallName))
-            NShopSearch.search(searchItems, object : NShopSearch.Listener {
-                override fun onPrepare() {
-                    viewHolder.itemView?.findViewById(R.id.imgButtonUpdate)
-                            ?.startAnimation(AnimationUtils.loadAnimation(context, R.anim.rotate))
-                }
-
-                override fun onComplete(results: List<SearchItem>?) {
-                    viewHolder.itemView?.findViewById(R.id.imgButtonUpdate)?.clearAnimation()
-                    SearchItemRepository.saveAll(results)
-                }
-
-            })
         }
 
 
